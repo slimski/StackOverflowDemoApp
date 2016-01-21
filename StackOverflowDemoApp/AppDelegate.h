c0392fb5715bd005f95d2a0638bffe674c750572
Igor Nabokov <slimski@gmail.com>
2016-01-22 00:26:04 +0400

    initial commit

---
+++ StackOverflowDemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackOverflowDemoApp
+//
+//  Created by Igor Nabokov on 22.01.16.
+//  Copyright © 2016 Igor Nabokov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
