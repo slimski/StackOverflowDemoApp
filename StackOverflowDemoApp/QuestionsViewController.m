758452a390094c449ca2e94e48b201f7ff760f11
Igor Nabokov <slimski@gmail.com>
2016-01-25 00:52:49 +0400

    added some style

--- StackOverflowDemoApp/QuestionsViewController.m
+++ StackOverflowDemoApp/QuestionsViewController.m
@@ -25,6 +25,8 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.tableView.estimatedRowHeight = 120;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
 }
 
 - (void)didReceiveMemoryWarning {
